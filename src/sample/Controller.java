16e4662d53423483feb064b36c5d5c27357d3bf4
kaiantiago <kaiantiago@outlook.com>
2020-09-20 20:55:08 -0300

    telas3

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,18 @@
 package sample;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class Controller {
+    public void abreCriarInfográfico()throws Exception{
+
+        Parent root = FXMLLoader.load(getClass().getResource("CriarInfográfico.fxml"));
+        Stage primaryStage = new Stage();
+        primaryStage.setTitle("Criar Infográfico");
+        primaryStage.setScene(new Scene(root, 790.0, 517.0));
+        primaryStage.show();
+    }
 }
+
--- src/sample/CriarInfográfico.java
+++ src/sample/CriarInfográfico.java
@@ -1,4 +1,36 @@
 package sample;
 
-public class CriarInfográfico {
+import javafx.embed.swing.SwingFXUtils;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CriarInfográfico implements Initializable {
+    public void carregaImagem() throws IOException {
+        JFileChooser chooseFile = new JFileChooser();
+        JPanel test = new JPanel();
+        chooseFile.setCurrentDirectory(new File(System.getProperty("user.home")));
+        int result = chooseFile.showOpenDialog(test);
+
+        if (result == JFileChooser.APPROVE_OPTION) {
+            File selectedFile = chooseFile.getSelectedFile();
+            BufferedImage bufferedImage = ImageIO.read(selectedFile);
+            ImageView myImageView = new ImageView();
+            Image image = SwingFXUtils.toFXImage(bufferedImage, null);
+            myImageView.setImage(image);
+
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
