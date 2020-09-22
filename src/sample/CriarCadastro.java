3db8970046cb67d3d2c3c2b9de636973d5772f65
hallefalves <hallef28@gmail.com>
2020-09-22 15:54:19 -0300

    Mudanças Cadastro.fxml

--- src/sample/CriarCadastro.java
+++ src/sample/CriarCadastro.java
@@ -2,19 +2,25 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.Window;
 
 import java.io.IOException;
 
-public class Cadastro {
+public class CriarCadastro {
     public void cadastraUser(ActionEvent actionEvent) throws IOException {
             Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
             Stage primaryStage = new Stage();
             primaryStage.setTitle("Login");
             primaryStage.setScene(new Scene(root, 790.0, 517.0));
             primaryStage.show();
+            final Node source = (Node) actionEvent.getSource();
+            final Stage stage = (Stage) source.getScene().getWindow();
+            stage.close();
+            }
         }
-    }
+
 
